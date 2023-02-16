bca71aa3e1bba1e1c35933aa45b38844bce62bf9
4lessandrodev <alessandroadm@live.com>
2023-02-16 11:17:42 -0300

    fix: validate args

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,17 @@ All notable changes to this project will be documented in this file.
 ## Unreleased
 
 ---
+
+### [0.0.2] - 2023-02-15
+
+### Fix
+
+- Fix: validate args
+- Fix: ensure min interval to 5000 ms
+- Fix: ensure import default
+
+---
+
 ### [0.0.1] - 2023-02-15
 
 ### Feat
--- tests/info.spec.ts
+++ tests/info.spec.ts
@@ -6,7 +6,7 @@ describe('info', () => {
     const result = { data: {}, status: 400 };
 
     const request = { socket: { remoteAddress: '::1' }, path: '/index', method: 'GET' } as Request;
-    
+
     const response = {
         status: function (status: number) {
             return ({
@@ -14,13 +14,13 @@ describe('info', () => {
                     result.data = data;
                     result.status = status;
                 }
-            })
+            });
         }
     } as Response;
 
     const next = function () {
         result.status = 200;
-        result.data = { next: true }
+        result.data = { next: true };
     } satisfies NextFunction;
 
     const ResetMock = () => { result.data = {}; result.status = 200; };
@@ -37,5 +37,15 @@ describe('info', () => {
         const middleware = blockDDoS();
         middleware(request, response, next);
         expect(result).toEqual({ status: 403, data: { message: 'Blocked by proxy. Try again in a moment!' } });
+    });
+
+    it('should throws if provide a time as string', () => {
+        const fn = () => blockDDoS('9000' as any);
+        expect(fn).toThrowError('The time interval must be a number');
+    });
+
+    it('should throws if provide a time less than 5000ms', () => {
+        const fn = () => blockDDoS(4000);
+        expect(fn).toThrowError('The time interval must be greater than or equal to 5000ms');
     });
 });
